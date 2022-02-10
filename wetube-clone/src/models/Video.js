e9b717ceca969478f868578f30c3c0b826147af2
Heochan <heochan01@korea.ac.kr>
2022-02-10 16:07:53 +0900

    Wetube Clone - MongoDB and Mongoose, etc.

--- wetube-clone/src/models/Video.js
+++ wetube-clone/src/models/Video.js
@@ -1,16 +1,25 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  // Date.now 뒤에 '()' 붙이면 함수가 즉시 실행된다. 안 붙이면 mongoose가 알아서 해줌.
+  hashtags: [{ type: String, trim: true }],
   metaData: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
   },
 });
 // 먼저, 데이터의 형식(스키마)을 정의한다. 
+
+videoSchema.static('formatHashtags', function(hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) =>
+      word.trim().startsWith("#") ? word.trim() : `#${word.trim()}`);
+});
+// static 함수를 만든다. (즉, Video.formatHashtags() 함수를 만든다.)
 
 const Video = mongoose.model("Video", videoSchema);
 // 위에 만들어 둔 스키마를 가지고 model을 만든다. (모델 이름, 스키마) 전달해 주기.
--- wetube-clone/src/routers/videoRouter.js
+++ wetube-clone/src/routers/videoRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { getUpload, postUpload, watch, getEdit, postEdit, remove } from "../controllers/videoController";
+import { getUpload, postUpload, watch, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
 
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); 
+videoRouter.get("/:id([0-9a-f]{24})", watch); 
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/remove", remove);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 
 
 export default videoRouter;

2511b4f1cf6dcf5f638891ed7fda5e686fdba3f0
Heochan <heochan01@korea.ac.kr>
2022-02-16 16:50:37 +0900

    Zoom Clone - Chat with WebSockets

--- zoom-clone/src/public/js/app.js
+++ zoom-clone/src/public/js/app.js
@@ -1,1 +1,46 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+// front-end가 가지고 있는 socket
+// 이 socket으로 backend와 메세지를 주고받을 수 있다.
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+
+const handleNNSubmit = (event) => {
+  event.preventDefault();
+
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+};
+
+const handleMsgSubmit = (event) => {
+  event.preventDefault();
+
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("chat", input.value));
+  input.value = "";
+};
+
+nicknameForm.addEventListener("submit", handleNNSubmit);
+messageForm.addEventListener("submit", handleMsgSubmit);
--- zoom-clone/src/server.js
+++ zoom-clone/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -12,4 +14,38 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("✅ Listening on : http://localhost:3000");
 
-app.listen(3000, handleListen);
+const httpServer = http.createServer(app);
+// express application을 이용해서 http 서버를 만든다.
+
+const wsServer = new WebSocketServer({ server: httpServer });
+// http 서버 위에 ws 서버를 만든다.
+// 항상 http 서버를 전달해야 하는 것은 아님. ws 서버만 만들 수도 있다.
+// http 서버를 전달하면 동일한 포트에서 http, ws request 두 개를 다 처리할 수 있다.
+
+const sockets = [];
+
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "익명";
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+
+  socket.on("close", () => {
+    console.log("Disconnected from Browser ❌");
+  });
+
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "chat":
+        sockets.forEach((aSocket) => 
+          aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break; 
+    } 
+  });
+});
+// "on" method의 callback : backend에 연결된 사람의 정보(socket)를 제공해 준다. 
+
+httpServer.listen(3000, handleListen);

fda37fe86675443a7583248afd11bbb0dcfbe454
Chan <heochan01@korea.ac.kr>
2023-02-08 11:59:44 +0000

    Effect - introduce useEffect()

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,22 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  // component state가 변경될 때마다 매번 실행되는 코드
+  console.log("I run all the time.");
+
+  // useEffect()를 사용, 맨 처음 render될 때만 실행되는 코드
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Create React App!</h1>
-      <Button text="Hello!" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
     </div>
   );
 }

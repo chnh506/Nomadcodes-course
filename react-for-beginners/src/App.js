746122492bd1e96d361c698dc8e74035f5fba11c
Chan <heochan01@korea.ac.kr>
2023-02-09 14:15:30 +0000

    Effect - Deps practice

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState();
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
 
   // component state가 변경될 때마다 매번 실행되는 코드
   console.log("I run all the time.");
@@ -13,8 +17,29 @@ function App() {
   };
   useEffect(iRunOnlyOnce, []);
 
+  // counter 변수가 바뀔 때에만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+
+  // keyword 변수가 바뀔 때에만 실행
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+
+  // counter 변수 혹은 keyword 변수가 바뀔 때에만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' or 'keyword' changes");
+  }, [counter, keyword]);
+
   return (
     <div>
+      <input
+        type="text"
+        value={keyword}
+        onChange={onChange}
+        placeholder="please put your keyword in."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me!</button>
     </div>

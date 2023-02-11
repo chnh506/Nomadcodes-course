f4e05e60372f13d710ac8494792f4ed687929334
Chan <heochan01@korea.ac.kr>
2023-02-11 14:50:24 +0000

    Ch07 - simple todo-list practice

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,47 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState();
-  const onClick = () => setCounter((prev) => prev + 1);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
   const onChange = (event) => {
-    setKeyword(event.target.value);
+    setTodo(event.target.value);
   };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") return;
 
-  // component state가 변경될 때마다 매번 실행되는 코드
-  console.log("I run all the time.");
-
-  // useEffect()를 사용, 맨 처음 render될 때만 실행되는 코드
-  const iRunOnlyOnce = () => {
-    console.log("I run only once.");
+    setTodos((curArr) => [todo, ...curArr]);
+    setTodo("");
   };
-  useEffect(iRunOnlyOnce, []);
-
-  // counter 변수가 바뀔 때에만 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-
-  // keyword 변수가 바뀔 때에만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-
-  // counter 변수 혹은 keyword 변수가 바뀔 때에만 실행
-  useEffect(() => {
-    console.log("I run when 'counter' or 'keyword' changes");
-  }, [counter, keyword]);
 
   return (
     <div>
-      <input
-        type="text"
-        value={keyword}
-        onChange={onChange}
-        placeholder="please put your keyword in."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me!</button>
+      <h1>My To-Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to dos..."
+        />
+        <button>Add To Do</button>
+      </form>
+
+      <hr />
+
+      <ul>
+        {todos.map((item, idx) => {
+          return <li key={idx}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 }

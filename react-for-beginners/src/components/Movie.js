6f65ab22e3d8249cb3628f9da795ac6d6a69d46c
Chan <heochan01@korea.ac.kr>
2023-02-20 14:48:55 +0000

    Code Challenge - reset.css & additional setup

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
+    <div className={styles.movieBox}>
       <img src={coverImg} alt={title} />
       <h2>
         <Link to={`/movie/${id}`}>

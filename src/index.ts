d9e0399dbe69190b3701d98a1d85dc7545d07373
Chan <heochan01@korea.ac.kr>
2023-06-21 13:44:13 +0000

    Typescript - Block class & better TS workflow(ts-node, DefinitelyTyped, etc.)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { init, exit } from "./mypackage";
+import crypto from "crypto";
 
-init({ debug: true, url: "true" });
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-exit(1);
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  // static 함수: 클래스 인스턴스가 없어도 부를 수 있는 함수
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}

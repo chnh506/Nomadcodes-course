ecfe7147ab36561e337eff3df42a56bff3fa0cec
Chan <heochan01@korea.ac.kr>
2023-06-26 05:14:35 +0000

    Typescript - Blockchain course CLEAR!

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ interface BlockShape {
 class Block implements BlockShape {
   public hash: string;
   constructor(
-    public prevHash: string,
-    public height: number,
-    public data: string
+    public readonly prevHash: string,
+    public readonly height: number,
+    public readonly data: string
   ) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
@@ -23,3 +23,37 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    else return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  // return type에 readonly를 붙여 보안상의 취약점을 해결했다.
+  public getBlocks(): readonly Block[] {
+    return this.blocks;
+  }
+}
+
+const myBlockchain = new Blockchain();
+myBlockchain.addBlock("First one");
+myBlockchain.addBlock("Second one");
+myBlockchain.addBlock("Third one");
+
+// myBlockchain.getBlocks().push(new Block("xxxxx", 11111, "HACKEDDDD"));
+console.log(myBlockchain.getBlocks());

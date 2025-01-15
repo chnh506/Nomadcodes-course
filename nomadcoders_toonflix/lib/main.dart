56d98869214d10e826efa7ee2b14f386cec9eaf0
Heochan <heochan01@korea.ac.kr>
2025-01-15 23:24:21 +0900

    Feat-toonflix: StatefulWidget initiation

--- nomadcoders_toonflix/lib/main.dart
+++ nomadcoders_toonflix/lib/main.dart
@@ -1,152 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:nomadcoders_toonflix/widgets/Button.dart';
-import 'package:nomadcoders_toonflix/widgets/currencyCard.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+// 첫 번째 부분: 위젯 그 자체. 우리의 어플리케이션 클래스로 확장되었다.
+// 매우 적은 양의 코드, State라고 불리는 것을 하나 가지고 있을 뿐이다.
+class App extends StatefulWidget {
   const App({super.key});
-  // StatelessWidget -> build() 메서드를 구현해야 한다.
-  // build 메서드 -> 우리 widget의 UI를 만드는 것
-  // flutter가 실행하게 될 메서드로, 이 메서드가 무엇을 return하던지 그것을 화면에 띄워줄 것이다.
 
-  // 이 App Widget은 runApp() 메서드에 주는 위젯이다. -> 즉, 우리 앱의 root이다.
-  // 모든 화면과 버튼 등등, 모든 것들이 App Widget으로부터 온다.
+  @override
+  State<App> createState() => _AppState();
+}
 
-  @override // Dart annotation. 부모 클래스에 있는 것을 override한다.
+// 두 번째 부분: 여기 있는 State
+// State는 우리가 UI를 구축하는 곳이고, 이 상태는 매우 특별하다.
+// 우리가 데이터를 바꿀 때, UI는 새로고침되면서 최신 데이터를 보여준다.
+// 데이터는 단순히 Dart의 클래스 프로퍼티일 뿐이다.
+class _AppState extends State<App> {
+  int counter = 0;
+
+  // IconButton 위젯의 onPressed 프로퍼티에 등록할 함수(메서드)
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Color(0xFF181818),
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 80,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withAlpha(204),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+              ),
+              Text(
+                '$counter',
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                SizedBox(
-                  height: 80,
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
                 ),
-                Text(
-                  'Total Balance',
-                  style: TextStyle(
-                    color: Colors.white.withAlpha(204),
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  '\$5 194 482',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 40,
-                  ),
-                ),
-                SizedBox(
-                  height: 28,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF1B33B),
-                      textColor: Colors.black,
-                    ),
-                    const Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withAlpha(204),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Currencycard(
-                  name: 'Euro',
-                  code: 'EUR',
-                  amount: '6 428',
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  order: 0,
-                ),
-                Currencycard(
-                  name: 'Bitcoin',
-                  code: 'BTC',
-                  amount: '9 785',
-                  icon: Icons.currency_bitcoin_rounded,
-                  isInverted: true,
-                  order: 1,
-                ),
-                Currencycard(
-                  name: 'Dollar',
-                  code: 'USD',
-                  amount: '110 000',
-                  icon: Icons.attach_money_rounded,
-                  isInverted: false,
-                  order: 2,
-                ),
-              ],
-            )),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 }

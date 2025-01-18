0fba904b9660accc7d528fd801f6f99f51297bce
Heochan <heochan01@korea.ac.kr>
2025-01-19 03:32:50 +0900

    Feat-toonflix: 웹툰 Detail Screen 생성 & Navigator 활용한 스크린 이동 구현 - GestureDetector - onTap -> Navigator 활용 - MaterialPageRoute로 route 생성 - Hero widget 애니메이션 alclssja

---
+++ nomadcoders_toonflix/lib/screens/detail_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String id, thumb, title;
+  const DetailScreen({
+    super.key,
+    required this.id,
+    required this.thumb,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: Offset(10, 10),
+                        color: Colors.black.withAlpha(75),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(thumb, headers: const {
+                    "User-Agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- nomadcoders_toonflix/lib/widgets/webtoon_widget.dart
+++ nomadcoders_toonflix/lib/widgets/webtoon_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nomadcoders_toonflix/models/webtoon_model.dart';
+import 'package:nomadcoders_toonflix/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final WebtoonModel webtoon;
@@ -10,36 +11,54 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 250,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: Offset(10, 10),
-                color: Colors.black.withAlpha(75),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              id: webtoon.id,
+              thumb: webtoon.thumb,
+              title: webtoon.title,
+            ),
+            fullscreenDialog: true,
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          Hero(
+            tag: webtoon.id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withAlpha(75),
+                  ),
+                ],
               ),
-            ],
+              child: Image.network(webtoon.thumb, headers: const {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              }),
+            ),
           ),
-          child: Image.network(webtoon.thumb, headers: const {
-            "User-Agent":
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-          }),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(
-          webtoon.title,
-          style: TextStyle(
-            fontSize: 20,
+          SizedBox(
+            height: 10,
           ),
-        ),
-      ],
+          Text(
+            webtoon.title,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

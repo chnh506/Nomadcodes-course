3ae9516f83f2cc86b031b4b8275cfc0085202c44
Heochan <heochan01@korea.ac.kr>
2025-01-19 22:04:52 +0900

    Feat-toonflix: 웹툰 디테일 & 에피소드 API 연동 - 두 개의 새로운 model 생성 - ApiService class에서 새로운 http get method 구현 - Detail Screen에서 Future의 형태로 정보 받아오기 구현

--- nomadcoders_toonflix/lib/screens/detail_screen.dart
+++ nomadcoders_toonflix/lib/screens/detail_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nomadcoders_toonflix/models/webtoon_detail_model.dart';
+import 'package:nomadcoders_toonflix/models/webtoon_episode_model.dart';
+import 'package:nomadcoders_toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String id, thumb, title;
+
   const DetailScreen({
     super.key,
     required this.id,
@@ -10,12 +14,30 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoonDetail; // 나중에 정의할 것이라 선언해 둔다.
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    // initState() 메서드에서 webtoonDetail을 초기화한다.
+    // data는 StatefulWidget인 DetailScreen로 전달된다.
+    // widget.id를 통해 State에서 id를 참조한다.
+    super.initState();
+    webtoonDetail = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
-          title,
+          widget.title,
           style: TextStyle(
             fontSize: 24,
           ),
@@ -35,7 +57,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -49,7 +71,7 @@ class DetailScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  child: Image.network(thumb, headers: const {
+                  child: Image.network(widget.thumb, headers: const {
                     "User-Agent":
                         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
                   }),
--- nomadcoders_toonflix/lib/services/api_service.dart
+++ nomadcoders_toonflix/lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:nomadcoders_toonflix/models/webtoon_detail_model.dart';
+import 'package:nomadcoders_toonflix/models/webtoon_episode_model.dart';
 import 'package:nomadcoders_toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -20,6 +22,35 @@ class ApiService {
       }
 
       return webtoonInstances;
+    } else {
+      throw Error();
+    }
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final uri = Uri.parse("$baseUrl/$id");
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> webtoon = jsonDecode(response
+          .body); // Parses the string and returns the resulting Json object.
+      return WebtoonDetailModel.fromJson(
+          webtoon); // Returns a new instance of WebtoonDetailModel.
+    } else {
+      throw Error();
+    }
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final uri = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final List<dynamic> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     } else {
       throw Error();
     }
